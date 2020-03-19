b5be279bfc59ed1d5528e2113644ed043750c765
canh <canhphan281094@gmail.com>
2020-03-19 23:48:16 +0700

    Updates

---
+++ precache-manifest.a677948de7b265314b6a15f4dfbe8465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fe2b368cd0214f1c0dc1fa17ceb32e",
+    "url": "/canhphan10994/calibretest/index.html"
+  },
+  {
+    "revision": "a6f605c41f5f11d5e4b4",
+    "url": "/canhphan10994/calibretest/static/css/2.d5abd7ea.chunk.css"
+  },
+  {
+    "revision": "0b45c898e1678ab8a34b",
+    "url": "/canhphan10994/calibretest/static/css/main.d938cbd6.chunk.css"
+  },
+  {
+    "revision": "a6f605c41f5f11d5e4b4",
+    "url": "/canhphan10994/calibretest/static/js/2.7f19e775.chunk.js"
+  },
+  {
+    "revision": "486c864dbeea4f82b0912c66e3826be4",
+    "url": "/canhphan10994/calibretest/static/js/2.7f19e775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b45c898e1678ab8a34b",
+    "url": "/canhphan10994/calibretest/static/js/main.499d37fd.chunk.js"
+  },
+  {
+    "revision": "55ec4141d7ea54b852c4",
+    "url": "/canhphan10994/calibretest/static/js/runtime-main.b7e7d022.js"
+  }
+]);
